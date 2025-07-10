0fa437c36f5d756ce7fe6ffa46955f2a14e9b87b
Kirill Stetsenko <stetsenkoki@gmail.com>
2025-07-11 01:24:37 +0300

    prepare remove image optimizer

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,10 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    ViteImageOptimizer({
-      jpg: {
-        quality: 50,
-      },
-    }),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
